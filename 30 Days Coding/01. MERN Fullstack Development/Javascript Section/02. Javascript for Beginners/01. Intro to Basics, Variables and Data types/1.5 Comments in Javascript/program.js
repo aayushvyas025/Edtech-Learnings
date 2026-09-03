cc3b3f2605da0cf2cd0cf265ee8d338b436dfce4
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-03 19:27:30 +0530

    Multi-line comment in Javascript

---
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/01. Intro to Basics, Variables and Data types/1.5 Comments in Javascript/program.js
@@ -0,0 +1,13 @@
+//* Comments in Javascript 
+/** 
+ * Comments are very useful features of programming language which helps to understand hints and features description of code  which help developers to work with collaborative environment in organization.  
+ * 
+ * * Comments are of two types 
+ *  1. Single line Comment 
+ *  In javascript we create one line or single line comment with // double forward slash  
+ *  2. Multiple line Comment 
+ *  In Javascript we create multi-line comment with the help of `forward slash` + `* asterisk`  
+ *    This is whole Description is written inside multi-line comment
+ */
+
+
