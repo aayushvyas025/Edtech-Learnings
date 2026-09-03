a3619bf212cde1f22b759ce7e73126c6a8c025a8
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-03 17:15:52 +0530

    Javascript Introduction

---
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/01. Intro to Basics, Variables and Data types/1.1 Introduction to Javascript/program.js
@@ -0,0 +1,12 @@
+//* Introduction to Javascript  
+
+/** 
+ *? What is Javascript ?  
+ *  -> Javascript is high level Prototypal Inheritance based programming language basically used to create webpages, dynamic, adding interactive effects like animation and interactive within the web browsers without refresh pages. 
+ *  ->  Nowadays Javascript is used for beyond the web browsers like for backend development, desktop development, mobile-application development, frontend development etc. 
+ * 
+ */   
+
+ 
+
+
