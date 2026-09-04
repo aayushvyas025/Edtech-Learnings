a69f26da66629a42848fdc4c7e46d0dd24f4e87a
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-04 23:51:51 +0530

    Introduction to operators and arithmetic operators

---
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/02. Operators and functions in Javscript/2.1 Operators in Javascript/program.js
@@ -0,0 +1,43 @@
+//* Operators in Javascript
+/**
+ * - Operators are used in javascript to perform various type of operation on program data.
+ * - In Javascript we have various type of operators to perform operation.
+ *
+ * ?   What is Operand ?
+ *    - Operands are the value or variable on which or between them operators perform operation
+ */
+
+//? Arithmetic operators
+/**
+ * To perform mathematical operations we used arithmetic operators
+ */
+
+// Addition arithmetic operator '+'
+let numOne = 58; // numOne variable
+let numTwo = 42; // numTwo variable
+
+const additionOperation = numOne + numTwo;
+console.log(additionOperation); // Output: 100  
+
+// Subtraction arithmetic operator '-' 
+const subtractionOperation = numOne - numTwo; 
+console.log(subtractionOperation); // Output: 16 
+
+// multiplication arithmetic operator `*` 
+const multiplicationOperation = numOne * numTwo; 
+console.log(multiplicationOperation);  // Output: 2436 
+
+// division arithmetic operator `/` 
+const divisionOperation = numOne / numTwo;  // Division operator returns quotient 
+console.log(divisionOperation); // Output: 1.380952380952381
+
+// Modulus arithmetic operator `%` 
+const remainderOperation = numOne % numTwo; // Modulus operator returns remainder but it work same as division, but here we use modulus
+console.log(remainderOperation); // Output: 16  
+
+// Exponential arithmetic operator `**` 
+const exponentialOperation = numOne ** 2;  // When we have to find power of any number than we use exponential operator 
+console.log(exponentialOperation); // Output: 3364 
+
+
+
