78afd93eabf6b873991d1c039c73163f69b84c5c
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-05 00:30:47 +0530

    unary operator with increment and decrement

--- 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/02. Operators and functions in Javscript/2.1 Operators in Javascript/program.js
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/02. Operators and functions in Javscript/2.1 Operators in Javascript/program.js
@@ -18,6 +18,9 @@ let numTwo = 42; // numTwo variable
 
 const additionOperation = numOne + numTwo;
 console.log(additionOperation); // Output: 100  
+/**
+ * ? Here numOne & numTwo are operands and + `addition symbol` is operator
+ */
 
 // Subtraction arithmetic operator '-' 
 const subtractionOperation = numOne - numTwo; 
@@ -39,5 +42,69 @@ console.log(remainderOperation); // Output: 16
 const exponentialOperation = numOne ** 2;  // When we have to find power of any number than we use exponential operator 
 console.log(exponentialOperation); // Output: 3364 
 
+//? Unary operator 
+/** 
+ * Unary operators are used to perform operation on unary or single operand. 
+ */  
 
+// Assignment operator `=`
+/** 
+ * We used  assignment operator to assign something or passing reference to another variable or perform operation on single operand only. 
+ */ 
 
+let num = 25 // Here '=' is unary operator 
+console.log(num); // Output: 25 
+
+// Arithmetic unary operator 
+/** 
+ * We can perform arithmetic operation on single operand also.  
+ */
+ num+= 5; // behind the scene num = 25 + 5 = 30 
+ console.log(num); // Output: 30 
+ num -= 10; // behind the scene num = 30 - 10 = 20 
+ console.log(num);  // Output: 20 
+ num *= 5; // behind the scene num = 20 * 5 = 100 
+ console.log(num); // Output: 100 
+ num /= 2; // behind the scene num = 100 / 2 = 50 
+ console.log(num); // Output: 50 
+ num **= 2;  // behind the scene num = 50 ^ 2 = 50 * 50 = 2500  
+ console.log(num); // Output: 2500 
+ num %= 2; // behind the scene num = 2500 % 2 = 0 
+ console.log(num); // Output: 0 
+
+ // Increment and decrement operator 
+ /** 
+  * increment and decrement operators are very handy for update value by 1 
+  */ 
+
+ // increment operator  
+ /** 
+  * increment operator will increase our value by 1 
+  * increment operator are also two types 
+  *  - pre-increment ++<operand> 
+  *   in pre-increment first value update than process 
+  *  - post-increment <operand>++  
+  *   in post-increment first value process than update 
+  */ 
+
+ // pre-increment operator example 
+ console.log(++num); // Output: 1 
+ // post-increment operator example 
+ console.log(num++); // Output: 1
+console.log(num); // Output: 2 
+
+// decrement operator 
+/** 
+ * decrement operator will decrease our value by 1 
+ * decrement operator are also two types 
+ * - pre-decrement --<operand> 
+ *   in pre-decrement first value update than process  
+ * - post-decrement <operand>-- 
+ *  in post-decrement first value process than update 
+ */
+ 
+// pre-decrement operator example 
+ console.log(--num); // Output: 1 
+ // post-decrement operator example 
+ console.log(num--); // Output: 1
+console.log(num); // Output: 0
