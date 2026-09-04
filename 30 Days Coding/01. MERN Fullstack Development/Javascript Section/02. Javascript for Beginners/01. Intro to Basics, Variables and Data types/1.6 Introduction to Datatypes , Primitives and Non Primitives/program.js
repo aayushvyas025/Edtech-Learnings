99579a59209b3f87fc7e1a222178ffda56c9f295
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-04 17:45:58 +0530

    Introduction to Datatypes, Primitives and Non-Primitives

---
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/01. Intro to Basics, Variables and Data types/1.6 Introduction to Datatypes , Primitives and Non Primitives/program.js
@@ -0,0 +1,20 @@
+//* Introduction to Datatypes, Primitives and Non Primitives 
+
+/** 
+ * ? Introduction to Datatypes 
+ *  - Programming language provides different types of data to store in memory for process of our program 
+ *  - This different types of data are classified into broad category according to their types, that's known as Datatype 
+ *  - negative, positive, fraction numbers are classified as broad category numbers and numeric characters, alphabetic characters, special characters are classified as string datatypes 
+ * 
+ * * Types of Datatype  
+ *  Datatypes are further classified into two or more types  
+ * * 1st. Primitive datatype 
+ *  - Primitive datatype are the basic and fundamental data which are inbuilt and used to store single datatype 
+ *  - Primitive datatype are immutable means can't change it's original value but we can pass as reference to other 
+ *  - Example of primitive : number, boolean, string etc. 
+ * 
+ * * 2nd. Non Primitive datatype 
+ *  - Non-primitive datatype are those which are derived from primitive that's why know as derived datatype also. 
+ *  - Derived datatypes are used to store more complex data like more than one primitive types and they are mutable in nature  
+ *  - Example of non -primitives: Objects are the non-primitives which are also of two types in-build object and user defined objects, and objects have also subset which are functions and arrays is special types of objects 
+ */
