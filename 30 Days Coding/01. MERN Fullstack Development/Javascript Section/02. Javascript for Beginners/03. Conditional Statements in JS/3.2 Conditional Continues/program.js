a8adc819ef3276ad74abbebb8b0bf569c0228f86
Aayush Vyas <aayush25vyas@gmail.com>
2026-09-06 16:18:47 +0530

    Nested if statement

--- 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/03. Conditional Statements in JS/3.2 Conditional Continues/program.js
+++ 30 Days Coding/01. MERN Fullstack Development/Javascript Section/02. Javascript for Beginners/03. Conditional Statements in JS/3.2 Conditional Continues/program.js
@@ -1,1 +1,31 @@
-//* Conditional Continues 
+//* Conditional Continues
+
+//? Nested if Statements
+/**
+ * Nested if-statements
+ *  - We can create if-statement inside the if-statement than it called as Nested if Statement
+ *
+ */
+
+//* Example of Nested-if Statement
+
+let age = 19;
+let citizen = "American";
+
+// case 
+citizen = "Indian"; 
+
+// Creating condition of casting vote in india
+
+if (age >= 18) {
+  // Here we created nested if-statement to check citizen
+  if (citizen === "Indian") {
+    console.log("You are eligible to cast your vote");
+  } else {
+    console.log("You are not Indian");
+  }
+} else {
+  console.log("You are not eligible to cast your vote");
+}
+
+
